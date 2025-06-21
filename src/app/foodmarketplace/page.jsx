d894db26c3cb97f2257f0095d452175dc6a3f4f9
NikhilThakur3536 <thakurnikhil657@gmail.com>
2025-06-21 23:36:38 +0530

    Nav Bar position fixed

--- src/app/foodmarketplace/page.jsx
+++ src/app/foodmarketplace/page.jsx
@@ -3,8 +3,8 @@ import FoodNavBar from "../components/foodmarketplace/NavBar";
 export default function FoodMaretPlace(){
     return(
         <div className="w-screen h-screen flex justify-center">
-            <div className="max-w-md w-full relative bg-rose-50 h-screen">
-                <div className="absolute bottom-0 z-20 backdrop-blur-xs w-full">
+            <div className="max-w-md w-full relative h-screen">
+                <div className="fixed bottom-0 z-20 backdrop-blur-xs w-full">
                     <FoodNavBar/>
                 </div>
             </div>
